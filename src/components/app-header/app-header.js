564200e602f80d6b0216607912399136bf02f47b
Anton Kurdin <anton.kurdin@gmail.com>
2023-12-21 23:13:24 +0300

    sprint-1/sprint-2: Удалён неактуальный cra код. Создана файловая структура проекта. Добавлен компонент AppHeader

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import appHeaderStyles from './app-header.module.css'
+import { Logo, BurgerIcon, ListIcon, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components'
+
+const AppHeader = () => {
+    const NavItem = ({children, text}) => {
+        return (
+            <div className={`${appHeaderStyles['app-header__nav-item']} mr-2 pt-4 pr-5 pb-4 pl-5 `}>
+                {children}
+                <p className="text text_type_main-default ml-2">{text}</p>
+            </div>
+        )
+    }
+
+    return (
+        <header className={`${appHeaderStyles['app-header']} pt-4 pb-4`}>
+            <nav className={appHeaderStyles['app-header__nav']}>
+                <div className={appHeaderStyles['app-header__nav-wrapper']}>
+                    <NavItem text="Конструктор">
+                        <BurgerIcon type='primary'/>
+                    </NavItem>
+
+                    <NavItem text="Лента заказов">
+                        <ListIcon type='primary'/>
+                    </NavItem>
+                </div>
+
+                <Logo/>
+
+                <div className={appHeaderStyles['app-header__nav-wrapper']}>
+                    <NavItem text="Лента заказов">
+                        <ProfileIcon type="primary"/>
+                    </NavItem>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default AppHeader;
