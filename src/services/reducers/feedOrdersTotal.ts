39d721f9749e5fe09afc378f4580200052856b70
Anton Kurdin <anton.kurdin@gmail.com>
2024-03-04 22:14:31 +0300

    sprint-5/step-1: добавлен редьюсер ленты заказов

---
+++ src/services/reducers/feedOrdersTotal.ts
@@ -0,0 +1,41 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import { TFeedOrders } from "../../utils/types";
+
+export type TFeedOrdersTotalStore = {
+    wsConnected: boolean,
+    data: TFeedOrders | null,
+    error: string | null,
+}
+
+const initialState: TFeedOrdersTotalStore  = {
+    wsConnected: false,
+    data: null,
+    error: null,
+}
+
+const feedOrdersTotalSlice = createSlice({
+    name: 'feedOrdersTotal',
+    initialState,
+    reducers: {
+        wsOpen: (state) => {
+            state.wsConnected = true;
+            state.error = null;
+        },
+        wsError: (state, action: PayloadAction<string>) => {
+            state.error = action.payload;
+        },
+        wsClose: (state) => {
+            state.wsConnected = false;
+        },
+        wsMessage: (state, action: PayloadAction<TFeedOrders>) => {
+            state.data = action.payload;
+        },
+    }
+})
+
+export const reducer = feedOrdersTotalSlice.reducer;
+export const {wsOpen, wsError, wsClose, wsMessage} = feedOrdersTotalSlice.actions;
+
+type TFeedOrdersTotalActionCreators = typeof feedOrdersTotalSlice.actions;
+
+export type TFeedOrdersTotalActions = ReturnType<TFeedOrdersTotalActionCreators[keyof TFeedOrdersTotalActionCreators]>
